43b2d24597d19229f60d4e42bb9e3ef7ab55401d
Mihai Boisteanu <mihai.boisteanu@gmail.com>
2025-01-29 11:48:55 +0200

    Implement Bottom Navigation with Fragments
    - Added BottomNavigationView in MainActivity
    - Created Home, Dashboard, and Profile fragments
    - Defined navigation graph (nav_graph.xml) for fragment transitions
    - Connected BottomNavigationView with NavController
    - Added menu items and icons for navigation

---
+++ app/src/main/java/com/mihudevcom/androidplayground/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.mihudevcom.androidplayground
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+    }
+}
